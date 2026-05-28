ecfb4d9ae768042d3e676bda6a2dda61f3d230f1
Doniyor <doniyorr1910@gmail.com>
2026-05-29 03:23:48 +0500

    Fix typo in __all__ export

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -3,7 +3,7 @@ from app.database.connection import get_connection
 from app.database.queries import check_duplicate, insert_document, insert_chunks
 
 __all__ = [
-    "get_cursor",
+    "get_connection",
     "check_duplicate",
     "insert_document",
     "insert_chunks",
